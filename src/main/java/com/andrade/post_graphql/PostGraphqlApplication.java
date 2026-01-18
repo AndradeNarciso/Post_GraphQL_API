24e984d077c8dd52a72f3f2eda8d15ae68e8d0c6
AndradeNarciso <andradegregorio277@gmail.com>
2026-01-18 22:05:10 +0200

    Loading project

---
+++ src/main/java/com/andrade/post_graphql/PostGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.andrade.post_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostGraphqlApplication.class, args);
+	}
+
+}
